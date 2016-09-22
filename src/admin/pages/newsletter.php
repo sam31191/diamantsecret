07e16fd672eb03875516da645678840f8cfe5bac
Ryan Bhanwra <ryan.bhanwra@yahoo.com>
2016-09-23 04:24:29 +0530

    Minimal change

--- src/admin/pages/newsletter.php
+++ src/admin/pages/newsletter.php
@@ -47,7 +47,7 @@ include '../../conf/config.php';
             'advlist autolink lists link image charmap print preview hr anchor pagebreak',
             'searchreplace wordcount visualblocks visualchars code fullscreen',
             'insertdatetime media nonbreaking save table contextmenu directionality',
-            'emoticons template paste textcolor colorpicker textpattern imagetools'
+            'emoticons paste textcolor colorpicker textpattern imagetools'
           ],
         height: "220px",
         toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
