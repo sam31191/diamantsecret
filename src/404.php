3e1c88f3465a7f425ee06d91adec4d749c635dc4
princebhanwra <princebhanwra@gmail.com>
2017-04-30 12:13:54 +0200

    added 404 page

---
+++ src/404.php
@@ -0,0 +1,76 @@
+<?php
+if ( session_status() == PHP_SESSION_NONE ) {
+	session_start();
+}
+include 'conf/config.php';
+?>
+<!doctype html>
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
+  <link rel="canonical" href="/" />
+  <meta name="description" content="" />
+  <title>Page not found</title>
+  
+    <link href="./assets/stylesheets/font.css" rel='stylesheet' type='text/css'>
+  
+	<link href="./assets/stylesheets/font-awesome.min.css" rel="stylesheet" type="text/css" media="all"> 	
+	<link href="./assets/stylesheets/bootstrap.min.3x.css" rel="stylesheet" type="text/css" media="all">
+	<link href="./assets/stylesheets/cs.bootstrap.3x.css" rel="stylesheet" type="text/css" media="all">
+	<link href="./assets/stylesheets/cs.animate.css" rel="stylesheet" type="text/css" media="all">
+	<link href="./assets/stylesheets/cs.global.css" rel="stylesheet" type="text/css" media="all">
+	<link href="./assets/stylesheets/cs.style.css" rel="stylesheet" type="text/css" media="all">
+	<link href="./assets/stylesheets/cs.media.3x.css" rel="stylesheet" type="text/css" media="all">
+  	<link href="./assets/stylesheets/site.css" rel="stylesheet" type="text/css" media="all">
+  	<link rel="icon" href="./images/gfx/favicon.png?v=1" type="image/png" sizes="16x16">
+	
+	<script src="./assets/javascripts/jquery-1.9.1.min.js" type="text/javascript"></script>
+	<script src="./assets/javascripts/bootstrap.min.3x.js" type="text/javascript"></script>
+</head>
+
+<body itemscope="" itemtype="http://schema.org/WebPage" class="templateCustomersRegister notouch">
+  
+	<!-- Header -->
+	<?php include './url/header.php'; ?>
+  
+	<div id="content-wrapper-parent">
+		<div id="content-wrapper">  
+			<!-- Content -->
+			<div id="content" class="clearfix">        
+				<div id="breadcrumb" class="breadcrumb">
+					<div itemprop="breadcrumb" class="container">
+						<div class="row">
+							<div class="col-md-24">
+								<a href="./index.php" class="homepage-link" title="Back to the frontpage">Home</a>
+								<span>/</span>
+								<span class="page-title">Page not found</span>
+							</div>
+						</div>
+					</div>
+				</div>              
+				<section class="content">
+					<div class="container">
+						<div class="row">
+							<div id="page-header" class="col-md-24">
+						
+								
+							</div>
+							<div id="col-main" class="col-md-24">
+							<h1>404 - Page not found</h1>
+							<h3 style="font: inherit;">Unfortunately the page you are looking for has been moved or deleted.</h3>
+							</div>   
+							<div>
+							<a href="./index.php" class="btn" style="margin: 10px 0px 0px 10px;">Go to homepage</a>
+							</div>
+						</div>
+					</div>
+				</section>        
+			</div>
+		</div>
+	</div>
+
+	<?php include './url/footer.php'; ?>
+</body>
